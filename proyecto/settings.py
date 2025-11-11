7c594e6218d0f5aa8bb7af3305926cf75c67f655
Nicolas Rivas Meneses <nicolasrivasmeneses23@gmail.com>
2025-11-11 15:01:46 -0500

    Correcion env

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uu%mujl_sw5axans&rrbp)3k0#n49+s##($6^q#!8d%gnb!q7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
